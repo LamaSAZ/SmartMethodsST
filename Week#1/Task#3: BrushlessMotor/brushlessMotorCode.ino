491db94ce6b265a3caaa9e1ff7045fd3f9d2c49d
LamaSAZ <108194917+LamaSAZ@users.noreply.github.com>
2022-07-16 19:08:47 +0300

    Update brushlessMotorCode.ino

--- Week#1/Task#3: BrushlessMotor/brushlessMotorCode.ino
+++ Week#1/Task#3: BrushlessMotor/brushlessMotorCode.ino
@@ -8,11 +8,11 @@ int potpin = A0;  // analog pin used to connect the potentiometer which will the
 int Bspeed;    // variable to read the value from the analog pin
 
 void setup() {
-  ESC.attach(11);  // attaches the servo on pin 9 to the servo object
+  ESC.attach(11);  // attaches the ESC on pin 9 to the servo object
 }
 
 void loop() {
   Bspeed = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  Bspeed = map(Bspeed, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
+  Bspeed = map(Bspeed, 0, 1023, 0, 180);     // scale it for use with the ESC (value between 0 and 180)
   ESC.write(Bspeed);                  // sets the brushless motor speed according to the scaled value of Bspeed variable
 }
